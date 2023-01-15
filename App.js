40cb846bc0bbfd679299f50d6e20259281727dd8
David Conley <dconley1989@gmail.com>
2023-01-15 11:42:00 -0700

    added general structure of landing page and made a network request for the button press and then displayed the data of the network request in the app

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
 
 export default function App() {
+  const [isloading, setIsLoading] = useState(false);
+  const [starships, setStarships] = useState([]);
+  const [starShipNames, setStarShipNames] = useState([]);
+
+  const handleViewStarships = () => {
+    fetch("https://swapi.dev/api/starships")
+      .then((resp) => resp.json())
+      .then((json) => {
+        setStarships(json.results);
+        const results = json.results;
+        console.log(results);
+        const names = results.map((starship) => {
+          return { ["starshipName"]: starship.name };
+        });
+        names.forEach((item, i) => (item["key"] = i + 1));
+        setStarShipNames(names);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to your Star Wars Starship Catalogue</Text>
+      <Button onPress={handleViewStarships} title="View Starships"></Button>
+      {starShipNames.length > 0 ? (
+        <FlatList
+          data={starShipNames}
+          renderItem={({ item }) => <Text>{item.starshipName}</Text>}
+        />
+      ) : null}
     </View>
   );
 }
@@ -13,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
